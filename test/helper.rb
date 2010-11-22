8efff2ef148e40741026deb581c0097eea790d8c
Moises Beltran <BMoises@gmail.com>
2010-11-22 07:30:33 -0800

    Updated code

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'chamel'
+require 'template_combinator'
 
 class Test::Unit::TestCase
 end
